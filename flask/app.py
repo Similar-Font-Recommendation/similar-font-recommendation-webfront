3ba5478fbe20c919ae1c2b3445a865c8843fbf3c
[username] <51512016+Eundongdong@users.noreply.github.com>
2023-01-13 00:16:52 +0900

    feat : flask server 추가 및 react연결 테스트

---
+++ flask/app.py
@@ -0,0 +1,145 @@
+import os
+import io
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from flask_cors import CORS
+# from flask_cors import CORS
+from flask import Flask, jsonify,request, render_template, url_for
+
+# Google OCR
+import platform
+from PIL import ImageFont, ImageDraw, Image
+from google.cloud import vision
+
+app = Flask(__name__)
+CORS(app)
+app.config['DEBUG'] = True
+#img file path config
+app.config['IMG_FOLDER'] = os.path.join('static','images') 
+
+# [START vision_text_detection]
+def detect_text(path):
+    """Detects text in the file."""
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
+
+    # [START vision_python_migration_text_detection]
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+    # [END vision_python_migration_text_detection]
+# [END vision_text_detection]
+
+
+# Members API Route
+
+@app.route('/home')
+def home():
+    path = './static/test2.jpeg'
+    detect_text(path)
+    return 'Hello world'
+
+
+
+
+'''fetch test : 리액트 log에 찍기'''
+@app.route('/api/users')
+def users():
+    return {"members": [{"id" : 1, "name" : "test"},
+                        {"id" : 2, "name" : "eunji"}]
+    }
+
+@app.route('/api/image', methods=['POST'])
+def image(file=None):
+    # parsed_request = request.files.get('file')
+    # fileName = request.form.get('name')
+
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'File is missing',404
+
+        # file 받기
+        pic_data = request.files['file']
+        #byte 단위로 읽어들이기
+        input_stream = io.BytesIO()
+        pic_data.save(input_stream)
+        data = np.fromstring(input_stream.getvalue(), dtype=np.uint8)
+        real_img = cv2.imdecode(data,1) # 컬러 사진
+        real_img = cv2.cvtColor(real_img,cv2.COLOR_BGR2GRAY)
+        
+        #이미지 확인하기
+        #plt.imshow(real_img)
+        #plt.show()
+
+        #OpenCV 이미지 이진화
+        max_output_value = 255
+        neighborhood_size = 99
+        subtract_from_mean = 10
+        image_binarized = cv2.adaptiveThreshold(real_img,
+                                                max_output_value,
+                                                cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                                cv2.THRESH_BINARY,
+                                                neighborhood_size,
+                                                subtract_from_mean)
+        kernel = np.ones((5,5),np.uint8)
+        opening = cv2.morphologyEx(real_img,cv2.MORPH_OPEN,kernel)
+
+        plt.imshow(opening, cmap='gray')
+        plt.show()
+
+        filename = pic_data.filename
+
+        #  cmd 창에서 확인하기
+        print(pic_data)
+        print(filename)
+
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        dir_path = dir_path +"\static"
+        saved_file_path = os.path.join(dir_path,filename)
+        pic_data.save(saved_file_path) #saved_file_path 경로에 받은 file 저장
+        
+        # openCV 테스트
+        # test = cv2.imread(filename,cv2.IMREAD_COLOR)
+        # print("###OpenCV Test### /n")
+
+        # path_cv = os.listdir(os.path.join(app.config['IMG_FOLDER'],filename))
+        # cv2.imwrite('./static/images/img1.jpg',test)
+        
+    return 'Good!'
+
+
+   
+
+
+
+'''POST 테스트 : '''
+@app.route('/api/post',methods=["POST"])
+def post_test():
+    name = request.args.get('name')
+    age = request.args.get('key')
+    
+    return "POST TEST RESULT :  %s" %name
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ react/src/App.js
@@ -0,0 +1,186 @@
+import './App.css';
+// import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Box,Paper, Grid,styled,AppBar,Toolbar,Typography, createTheme,ThemeProvider } from '@mui/material';
+import { Card, Button } from "react-bootstrap";
+import Dropzone from 'react-dropzone';
+import OCRImg from './assets/OCR_img.png';
+import Result1 from './assets/Result_1.png';
+import Result2 from './assets/Result_2.png';
+import Result3 from './assets/Result_3.png';
+import Result4 from './assets/Result_4.png';
+import Result5 from './assets/Result_5.png';
+
+import ResultTable from './components/ResultTable';
+
+const Img = styled('img')({
+  margin: 'auto',
+  display: 'block',
+  maxWidth: '100%',
+  maxHeight: '100%',
+})
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Yes24'
+  }
+})
+function App(){
+  const [visible,setVisible] = useState(false);
+
+  const [files, setFiles] = useState([])
+  const [data,setData]=useState()
+
+  // useEffect(() => {
+  //   fetch("/api/users",{
+  //     headers:{
+  //       'Content-Type': 'application/json',
+  //       'Accept' : 'application/json'
+  //     }
+  //   }).then(
+  //     // response 객체의 json() 이용하여 json 데이터를 객체로 변화
+  //     res => res.json()
+  //   ).then(
+  //     // 데이터를 콘솔에 출력
+  //     data => {
+  //       setData(data);
+  //       console.log(data);
+  //     }
+  //  ).catch(
+  //   (err) =>console.log("error: 발생 "+err)
+  //  )
+  // },[])
+
+  useEffect(()=>{
+    var canvas = document.getElementById("imageCanvas");
+    var ctx = canvas.getContext("2d"); 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var img = document.getElementById("preview");
+    canvas.width  = img.width;
+    canvas.height = img.height;
+    ctx.lineWidth = "5";
+    ctx.strokeStyle = "lightgreen";
+    var top = 24;
+    var bottom = 40;
+    var left = 472;
+    var right = 199;
+    ctx.rect(left, top, (right-left), (bottom-top));
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+    ctx.stroke();
+  })
+
+  async function onDrop(pic){    
+    const imgData = new FormData();
+    imgData.append("file",pic[0]);
+    await axios
+      .post("./api/image",imgData)
+      .then(console.log("onDrop 성공"))
+      .then(res=>{
+        console.log(res);
+      })
+      .catch((e)=>{
+        console.error(e);
+      });     
+    console.log("FormData 전송완료")
+    
+  }
+
+
+  //Post 예시
+  async function onClick(){
+    try{
+      await axios.post("/api/post",{
+        'name' : "ej",
+        'age' : 24
+      });
+      console.log("POST 완료")
+    }catch(e){
+      console.error(e);
+    }
+
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <AppBar position="static"style={{background: '#FFFFFF' ,padding:'1em'}}>
+        <Toolbar variant="dense">
+            <Typography variant="h4" color="black" component="div">
+            이미지 폰트 추천
+            </Typography>
+        </Toolbar>
+    </AppBar>
+      </ThemeProvider>
+      
+      <Grid container spacing={2} styles={{margin: 20, padding: 20  }}
+      position="static"
+      color="default"
+      >
+        <Grid xs={5} md={4} maxWidth="sm" sx={{ pt: 8, pb: 6 }} className="box" >
+        <Card style={{marginBottom:'1em'}}>
+        <Card.Header  as="h4" style={{padding:'0.6em'}}>이미지 가져오기</Card.Header>
+        <Card.Body>
+            <Card.Text>
+              <div className="DZ">
+                <Dropzone className="DZ"   multiple={false} onDrop={onDrop}>
+            {({getRootProps, getInputProps}) => (
+            <section>
+                <div {...getRootProps()}>
+                <input className="dropzone" {...getInputProps({type:'file', accept:'image/*'})} />
+                
+                <p style={{fontSize:'15pt'}}>업로드 할 이미지를 드래그하거나 박스를 <span style={{color:'lightBlue'}}> 클릭</span>하세요</p>
+                
+                
+                        </div>
+                    </section>
+                )}
+                </Dropzone>
+              </div>
+            
+            </Card.Text>
+               {/* <Card.Img src={Input} />  */}
+          </Card.Body>
+
+        </Card> 
+          <Card>
+            <Card.Header as="h4" style={{padding:'0.6em'}}>검색할 글자 선택하기</Card.Header>
+            <Card.Body>
+            <Card.Img  id='preview' style={{width: '70%'}}/>{'\n'}
+            <Grid container justifyContent="flex-end" >
+              <Button variant="outline-primary" style={{marginTop:'0.5em'}} onClick={()=>{
+                console.log("test");
+                setVisible(true);
+                    }}>
+                검색하기
+              </Button>
+            </Grid>
+            </Card.Body>
+          </Card>
+        </Grid>
+        <Grid xs={7} md={5}  maxWidth="sm" sx={{ pt: 8, pb: 6 }} className="box">
+          <Card>
+            <Card.Header as="h4" style={{padding:'0.6em', maxBlockSize:'800px'}}>검색 결과</Card.Header>
+            <Card.Body>
+            <Card.Title>선택한 글자</Card.Title>
+            {visible &&<ResultTable Result1={Result1} Result2={Result2} Result3={Result3} Result4={Result4} Result5={Result5} />}
+            </Card.Body>
+          </Card>
+        </Grid>
+      </Grid>
+      <div>
+        <h3>
+            {dummy.Block.map((Text)=>(
+              Text.x
+            ))}
+          </h3>
+        <img className="preview" id="preview" src={Img} />
+        <canvas className="imageCanvas" id="imageCanvas">이 브라우저는 'canvas'기능을 제공하지 않습니다.</canvas>
+      </div>
+      
+      
+    </>
+    );
+}
+
+export default App;
